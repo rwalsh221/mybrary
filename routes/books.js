0ed22fad2b21943050390be6baca0e1a2fdeafdf
Richard Walsh <richwalsh2@googlemail.com>
2022-01-20 17:38:13 +0000

    add file uploads

--- routes/books.js
+++ routes/books.js
@@ -1,22 +1,23 @@
 const express = require('express');
 const router = express.Router();
-const multer = require('multer');
-const path = require('path');
-const fs = require('fs');
+// const path = require('path');
+// const fs = require('fs');
 const Book = require('../models/book');
 const Author = require('../models/author');
 const { response } = require('express');
 const req = require('express/lib/request');
 const res = require('express/lib/response');
-const uploadPath = path.join('public', Book.coverImageBasePath);
-const imageMimeTypes = ['image/jpeg', 'image/png', 'image/gif', 'image/jpg'];
+// const uploadPath = path.join('public', Book.coverImageBasePath);
 
-const upload = multer({
-  dest: uploadPath,
-  fileFilter: (request, file, callback) => {
-    callback(null, imageMimeTypes.includes(file.mimetype));
-  },
-});
+const imageMimeTypes = ['image/jpeg', 'image/png', 'image/gif', 'image/jpg'];
+// FOR MULTER NO LONGER NEEDED
+
+// const upload = multer({
+//   dest: uploadPath,
+//   fileFilter: (request, file, callback) => {
+//     callback(null, imageMimeTypes.includes(file.mimetype));
+//   },
+// });
 
 const renderNewPage = async (response, book, hasError = false) => {
   try {
@@ -34,12 +35,24 @@ const renderNewPage = async (response, book, hasError = false) => {
 };
 
 // see 32:30  https://www.youtube.com/watch?v=Zi2UwhpooF8
-const removeBookCover = (fileName) => {
-  fs.unlink(path.join(uploadPath, fileName), (err) => {
-    if (err) {
-      console.error(err);
-    }
-  });
+// const removeBookCover = (fileName) => {
+//   fs.unlink(path.join(uploadPath, fileName), (err) => {
+//     if (err) {
+//       console.error(err);
+//     }
+//   });
+// };
+
+// https://youtu.be/Xm5MzWvklbI?t=709
+const saveCover = (book, coverEncoder) => {
+  if (coverEncoder == null) {
+    return;
+  }
+  const cover = JSON.parse(coverEncoder);
+  if (cover != null && imageMimeTypes.includes(cover.type)) {
+    book.coverImage = new Buffer.from(cover.data, 'base64');
+    book.coverImageType = cover.type;
+  }
 };
 
 // ***** ROUTES BELOW *******
@@ -84,25 +97,25 @@ router.get('/new', async (request, response) => {
 });
 
 // CREATE NEW BOOK ROUTE
-router.post('/', upload.single('cover'), async (request, response) => {
-  const fileName = request.file != null ? request.file.filename : null;
-
+router.post('/', async (request, response) => {
   const book = new Book({
     title: request.body.title,
     author: request.body.author,
     publishDate: new Date(request.body.publishDate),
     pageCount: request.body.pageCount,
-    coverImageName: fileName,
     description: request.body.description,
   });
+
+  saveCover(book, request.body.cover);
+
   try {
     const newBook = await book.save();
     // response.redirect(`books/${newbooks.id}`);
     response.redirect('books');
   } catch {
-    if (book.coverImageName != null) {
-      removeBookCover(book.coverImageName);
-    }
+    // if (book.coverImageName != null) {
+    //   removeBookCover(book.coverImageName);
+    // }
     renderNewPage(response, book, true);
   }
 });
